70f8636e85f8bbdebc4f1b091822f093f452fe78
nguyen ngoc dong <nguyen ngoc dong>
2017-04-26 19:39:32 +0700

    commit_1

---
+++ app/Task.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: nguyenngoc
+ * Date: 4/26/2017
+ * Time: 7:21 PM
+ */
+namespace App;
+use Illuminate\Database\Eloquent\Model;
+class Task extends Model
+{
+//
+}
